db0202920d2d3cb05f92a1039a2324948c9bc6e0
Dragutin Vujovic <dragutin.v@gmail.com>
2014-10-16 11:07:58 +0200

    Initial commit

---
+++ HW2/HW2/main.h
@@ -0,0 +1,10 @@
+#define PI 3.14159265
+int dumvar = 3;
+
+int MyFunction1(int, int);
+void MyFunction2(float);
+
+void ExampleInputOutput();
+void CartesianToPolar(int, int, float&, float&);
+void IsMultipleOf(int, int q = 2);
+bool isPrime(int, int);
